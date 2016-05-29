10a69ec0c384e76e80dc70775160503eb039f8fd
Javi <sanroman.javier@gmail.com>
2016-05-29 20:57:18 +0200

    Config codeclimate test reporter

--- Gemfile
+++ Gemfile
@@ -6,3 +6,8 @@ gemspec
 group :development, :test do
   gem "rspec"
 end
+
+group :test do
+  gem 'codeclimate-test-reporter', '~> 0.4.7'
+  gem 'simplecov', '~> 0.11.2'
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,17 @@
+begin
+  # Test coverage reporters
+  require 'codeclimate-test-reporter'
+  CodeClimate::TestReporter.start
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new [
+    SimpleCov::Formatter::HTMLFormatter,
+    CodeClimate::TestReporter::Formatter
+  ]
+  SimpleCov.start
+rescue LoadError
+  puts 'Codeclimate Test Reporter not installed. '\
+  'Install gem codeclimate-test-reporter for coverage data.'
+end
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'ga_collect'
 
